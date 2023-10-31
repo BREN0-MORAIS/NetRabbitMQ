a97158191a6ebd029a109db39c7e36afd652df72
BRENO MORAIS <62041808+BREN0-MORAIS@users.noreply.github.com>
2023-10-30 21:58:43 -0300

    #Feat creation  of producer and consumer.

---
+++ NetRabbitMQ/Consumer/Receiver.cs
@@ -0,0 +1,40 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+
+public static class Receiver { 
+
+
+    public  static void Conumer()
+    {
+        var factory = new ConnectionFactory
+        {
+
+            HostName = "localhost",
+            UserName = "guest",
+            Password = "guest"
+        };
+
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare("BrenoQueue", false, false, false, null);
+            var consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.Span;
+
+                var menssagem = Encoding.UTF8.GetString(body);
+                Console.WriteLine("Menssagem recebida"+ menssagem);
+            };
+
+            channel.BasicConsume("BrenoQueue", true, consumer);
+            Console.WriteLine("Precione [enter] para sair do consumer");
+
+        }
+
+    }
+
+}
---
+++ NetRabbitMQ/Prducer/Sender.cs
@@ -0,0 +1,36 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+
+public static class Sender
+{
+    public static void Publicar() {
+
+        var factory = new ConnectionFactory
+        {
+            HostName = "localhost",
+            UserName = "guest",
+            Password = "guest"
+        };
+
+
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare("BrenoQueue", false, false, false);
+
+            var message = "Bem vindo ao currso de  RabbitMQ em .NET";
+
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(
+                        exchange: "",
+                        routingKey: "BrenoQueue",
+                        basicProperties: null,
+                        body: body
+                    );
+
+            Console.WriteLine("Menssagem foi enviada");
+        }
+    }
+}
